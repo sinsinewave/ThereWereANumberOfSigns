743e66acc2eb1613c46dd02ed42612006f7b02fa
sinsinewave <Sigma1@tuta.io>
2025-06-21 01:02:42 +0300

    Move tint handling to SignsModClient instead of a dedicated object

--- src/main/kotlin/cyou/sinewave/signsmod/SignsModClient.kt
+++ src/main/kotlin/cyou/sinewave/signsmod/SignsModClient.kt
@@ -11,6 +11,7 @@ import net.neoforged.api.distmarker.Dist
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.client.event.EntityRenderersEvent.RegisterRenderers
+import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent
@@ -47,5 +48,29 @@ object SignsModClient {
     @SubscribeEvent
     fun registerScreens(event: RegisterMenuScreensEvent) {
         event.register(SignsModMenuTypes.DESIGN_TABLE.get(), ::DesignTableScreen)
+    }
+
+    @SubscribeEvent
+    fun handleItemTints(event: RegisterColorHandlersEvent.Item) {
+        for (item in SignsModItems.REGISTRY.entries) {
+            if (item.value() is ITinted) {
+                event.register(
+                    { stack, tintIndex -> (item.value() as ITinted).getColorRGB(tintIndex) },
+                    item.value()
+                )
+            }
+        }
+    }
+
+    @SubscribeEvent
+    fun handleBlockTints(event: RegisterColorHandlersEvent.Block) {
+        for (block in SignsModBlocks.REGISTRY.entries) {
+            if (block.value() is ITinted) {
+                event.register(
+                    { state, level, pos, tintIndex -> (block.value() as ITinted).getColorRGB(tintIndex) },
+                    block.value()
+                )
+            }
+        }
     }
 }
